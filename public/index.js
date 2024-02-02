4311a969fb17b6fe26b698477b2136ac5f79fe1e
RuiSiang <ruisiang2000@gmail.com>
2024-02-02 13:56:03 -0400

    chagne color theme and code structure

--- public/index.js
+++ public/index.js
@@ -1,118 +1,100 @@
-const socket = io()
-
-let loader = document.createElement("div")
-loader.classList.add("loader")
-
-function shuffle(array) {
-    let currentIndex = array.length, randomIndex;
-    while (currentIndex != 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-        [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
+class QuizPlatform {
+    constructor() {
+        this.socket = io();
+        this.initSocketEvents();
     }
 
-    return array;
-}
+    initSocketEvents() {
+        this.socket.on("question", question => this.handleQuestion(question));
+        this.socket.on("connected", () => this.handleConnected());
+        this.socket.on("correct", () => this.handleAnswerFeedback(true));
+        this.socket.on("incorrect", () => this.handleAnswerFeedback(false));
+        this.socket.on("noAnswer", () => this.handleNoAnswer());
+        this.socket.on("gameover", leaderboard => this.handleGameOver(leaderboard));
+    }
 
-socket.on("question", (question) => {
-
-    const shuffledAnswers = shuffle(question.answers)
-    swal({
-        title: question.text,
-        buttons: {
-            1: {
-                text: shuffledAnswers[0],
-                value: 1,
-            },
-            2: {
-                text: shuffledAnswers[1],
-                value: 2,
-            },
-            3: {
-                text: shuffledAnswers[2],
-                value: 3,
-            },
-            4: {
-                text: shuffledAnswers[3],
-                value: 4,
-            },
-        },
-        closeOnClickOutside: false,
-        closeOnEsc: false,
-    }).then(answer => {
-        socket.emit("answer", shuffledAnswers[answer - 1]) // We subtract 1 because arrays start at 0 and not 1
-        swal({
-            title: "Waiting for others",
-            buttons: false,
-            content: loader,
+    async handleConnected() {
+        const name = await swal("Your name:", {
+            content: "input",
+            button: "Join",
             closeOnClickOutside: false,
             closeOnEsc: false,
-        })
-    })
-})
-
-socket.on("connected", async _ => {
-    const name = await swal("Your name:", {
-        content: "input",
-        button: "Join",
-        closeOnClickOutside: false,
-        closeOnEsc: false,
-    })
-    socket.emit("name", name)
-    swal({
-        title: "Waiting for host",
-        buttons: false,
-        content: loader,
-        closeOnClickOutside: false,
-        closeOnEsc: false,
-    })
-})
-
-socket.on("correct", async _ => {
-    swal({
-        title: "Correct!",
-        text: "Keep it up :)",
-        icon: "success",
-        buttons: false,
-        closeOnClickOutside: false,
-        closeOnEsc: false,
-    })
-})
-
-socket.on("incorrect", async _ => {
-    swal({
-        title: "Incorrect!",
-        text: "Better luck next time :(",
-        icon: "error",
-        buttons: false,
-        closeOnClickOutside: false,
-        closeOnEsc: false,
-    })
-})
-
-socket.on("noAnswer", async _ => {
-    swal({
-        title: "Time's up!",
-        text: "You need to be quicker",
-        icon: "error",
-        buttons: false,
-        closeOnClickOutside: false,
-        closeOnEsc: false,
-    })
-})
-
-socket.on("gameover", async (leaderboard) => {
-    let leaderboardDisplay = document.createElement("ul")
-    for (player of leaderboard) {
-        leaderboardDisplay.innerHTML += `<li>${player[0]}: ${player[1]}</li>`
+        });
+        this.socket.emit("name", name);
+        this.showLoader("Waiting for host");
     }
-    swal({
-        title: "Game over!",
-        icon: "info",
-        content: leaderboardDisplay,
-        buttons: false,
-        closeOnClickOutside: false,
-        closeOnEsc: false,
-    })
-})
+
+    async handleQuestion(question) {
+        const shuffledAnswers = this.shuffleArray(question.answers);
+        const answer = await swal({
+            title: question.text,
+            buttons: this.createAnswerButtons(shuffledAnswers),
+            closeOnClickOutside: false,
+            closeOnEsc: false,
+        });
+        this.socket.emit("answer", shuffledAnswers[answer - 1]);
+        this.showLoader("Waiting for others");
+    }
+
+    handleAnswerFeedback(isCorrect) {
+        const title = isCorrect ? "Correct!" : "Incorrect!";
+        const text = isCorrect ? "Keep it up :)" : "Better luck next time :(";
+        const icon = isCorrect ? "success" : "error";
+        swal({ title, text, icon, buttons: false, closeOnClickOutside: false, closeOnEsc: false });
+    }
+
+    handleNoAnswer() {
+        swal({
+            title: "Time's up!",
+            text: "You need to be quicker",
+            icon: "error",
+            buttons: false,
+            closeOnClickOutside: false,
+            closeOnEsc: false,
+        });
+    }
+
+    async handleGameOver(leaderboard) {
+        let leaderboardDisplay = document.createElement("ul");
+        leaderboard.forEach(player => {
+            leaderboardDisplay.innerHTML += `<li>${player[0]}: ${player[1]}</li>`;
+        });
+        swal({
+            title: "Game over!",
+            icon: "info",
+            content: leaderboardDisplay,
+            buttons: false,
+            closeOnClickOutside: false,
+            closeOnEsc: false,
+        });
+    }
+
+    showLoader(message) {
+        const loader = this.createLoader();
+        swal({ title: message, buttons: false, content: loader, closeOnClickOutside: false, closeOnEsc: false });
+    }
+
+    createLoader() {
+        const loader = document.createElement("div");
+        loader.className = "loader";
+        return loader;
+    }
+
+    shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    createAnswerButtons(answers) {
+        return answers.reduce((buttons, answer, index) => {
+            buttons[index + 1] = { text: answer, value: index + 1 };
+            return buttons;
+        }, {});
+    }
+}
+
+const quizPlatform = new QuizPlatform();
+quizPlatform.initSocketEvents();
